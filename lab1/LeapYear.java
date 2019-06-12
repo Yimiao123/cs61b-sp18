e3bdd59f4ceabf6bc983dda7d20be8e5912e7af8
Yimiao123 <cao223@wisc.edu>
2019-06-12 03:24:25 -0500

    Completed second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -7,10 +7,10 @@ public class LeapYear {
      *  @param  year to be analyzed
      */
     public static boolean isLeapYear(int year){
-        if (year % 100 != 0 && year % 400 == 0){
+        if (year % 400 == 0){
             return true;
         }
-        else if (year % 4 == 0){
+        else if (year % 4 == 0 & year % 100 != 0){
             return true;
         }
         else{
